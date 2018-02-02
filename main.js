cc93fffde145c40d84d2c7ce3bf039c0267e4e02
Li Zhang <bylizhang@gmail.com>
2018-02-02 14:51:40 -0800

    added libraries

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 var the_number = null;
-$(document).ready(function(){
+$(document).ready(initializeApp);
+
+function initializeApp(){
 	alert('ok');
 	the_number = pick_number();
 	$('button').on('click', make_guess);
-});
+	addModalCloseHandler();
+}
 
 function pick_number() {
 	var random_number = Math.floor(Math.random()*10) + 1;
@@ -12,12 +15,11 @@ function pick_number() {
 
 function make_guess() {
 	var guess_input = $(this).text();
-	// var guess_input = document.querySelector('#guess_input').value;
 	var the_guess = Number(guess_input);
 	if (the_guess) {
 		if (the_guess > the_number) {
 			// $('#response_div').text('Too High!');
-			alert('too hight');
+			$('#galleryModal').modal();
 		} else if (the_guess < the_number) {
 			// $('#response_div').text('Too Low!');
 			alert('too low');
@@ -28,4 +30,16 @@ function make_guess() {
 	} else {
 		$('#response_div').text('Please only use a number between 1-10.');
 	}
+}
+
+function displayImage(){
+		$('h4.modal-title').text(fileNameMain[0]);
+		$('.modal-body>img').attr('src', 'images/'+fileName);
+		$('#galleryModal').modal();
+}
+
+function addModalCloseHandler(){
+	$('.modal-body>img').click(function(){
+		$('#galleryModal').modal('hide');
+	})
 }
