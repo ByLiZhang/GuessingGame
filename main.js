b3ddcaa7685411aeca4aec9a2209d1edf3b729a6
Li Zhang <bylizhang@gmail.com>
2018-02-03 01:01:50 -0800

    added redirect upon winning

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ function initializeApp(){
 	addModalCloseHandler();
 }
 
+function hoverOther() {
+	// body...
+}
+
 function pick_number() {
 	var random_number = Math.floor(Math.random()*10) + 1;
 	return random_number;
@@ -22,19 +26,19 @@ function make_guess() {
 	var modal_title = $('#galleryModal .modal-title');
 	if (the_guess) {
 		if (the_guess - the_number >= 2) {
-			modal_title.text('Power overwhelming!!! You\'re gonna blow this place into oblivion...');
+			modal_title.text('A mighty artifact that is! Blown into oblivion this place would be...');
 			image.attr('src', 'img/nuke.jpg');
-			modal_header.css('background-color', '#bd02d6')
+			modal_header.css('background-color', '#d600f7')
 			modal.modal();
 		} else if (the_guess - the_number === 1) {
-			modal_title.text('Nice hit! But sometimes less is more...');
-			image.attr('src', 'img/begentle.jpg');
-			modal_header.css('background-color', '#d64f02')
+			modal_title.text('Not a bad choice per se. But sometimes, SPEED is KING!');
+			image.attr('src', 'img/speed.gif');
+			modal_header.css('background-color', '#f7b900')
 			modal.modal();
 		} else if (the_guess - the_number <= -2) {
-			modal_title.text('Tis but a scratch! You may as well just use a toothpick!');
-			image.attr('src', 'img/blackknight.jpg');
-			modal_header.css('background-color', '#bd02d6')
+			modal_title.text('What was THAT?! You may as well just use a toothpick!');
+			image.attr('src', 'img/blackknight.gif');
+			modal_header.css('background-color', '#0c4db5')
 			modal.modal();
 		} else if (the_guess - the_number === -1) {
 			modal_title.text('A valiant effort! But Tywin wants you to try harder...');
@@ -42,10 +46,11 @@ function make_guess() {
 			modal_header.css('background-color', '#d64f02')
 			modal.modal();
 		} else {
-			modal_title.text('Amazing shot! You\'ve won!!!');
-			image.attr('src', 'img/chest.jpg');
+			modal_title.text('You did it!!! You\'ve done the impossible!!! That.Was.EPIC!!!');
+			image.attr('src', 'img/cheer.gif');
 			modal_header.css('background-color', '#d60202')
 			modal.modal();
+			setTimeout(redirect, 5000);
 		}
 	} else {
 		$('#response_div').text('Please only use a number between 1-10.');
@@ -62,4 +67,8 @@ function addModalCloseHandler(){
 	$('.modal-body>img').click(function(){
 		$('#galleryModal').modal('hide');
 	})
+}
+
+function redirect() {
+	window.open('file:///C:/Users/Samosa/Desktop/lfz/guess/home.html', '_blank');
 }
