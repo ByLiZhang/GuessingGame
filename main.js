ac80e5374ad150c90d7bc350062c49aabff59086
Li Zhang <bylizhang@gmail.com>
2018-02-03 21:15:07 -0800

    adjusted home page typewriter timing. added about page

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ function initializeApp(){
 	console.log(the_number);
 	$('button').on('click', make_guess);
 	addModalCloseHandler();
-}
-
-function hoverOther() {
-	// body...
+	$('#galleryModal .modal-title').text('Remember, balancing power and speed is KEY.  Picking the weapon that suits you the best shall help greatly.');
+	$('#galleryModal .modal-header').css('background-color', '#098c02');
+	$('.modal-body>img').attr('src', 'img/Dayne.gif');
+	$('#galleryModal').modal();
 }
 
 function pick_number() {
@@ -24,6 +24,7 @@ function make_guess() {
 	var modal = $('#galleryModal');
 	var modal_header = $('#galleryModal .modal-header');
 	var modal_title = $('#galleryModal .modal-title');
+	modal_header.css('background-color', '');
 	if (the_guess) {
 		if (the_guess - the_number >= 2) {
 			modal_title.text('Power overwhelming! Blown into oblivion this place would be...');
@@ -31,7 +32,7 @@ function make_guess() {
 			modal_header.css('background-color', '#d600f7')
 			modal.modal();
 		} else if (the_guess - the_number === 1) {
-			modal_title.text('Quite a nice choice. But sometimes, SPEED is KING!');
+			modal_title.text('A solid choice. But sometimes, swiftness is better!');
 			image.attr('src', 'img/speed.gif');
 			modal_header.css('background-color', '#f7b900')
 			modal.modal();
@@ -41,16 +42,16 @@ function make_guess() {
 			modal_header.css('background-color', '#0c4db5')
 			modal.modal();
 		} else if (the_guess - the_number === -1) {
-			modal_title.text('A valiant effort! But Tywin wants you to try harder...');
+			modal_title.text('A valiant effort! But Tywin wants you to try a little harder...');
 			image.attr('src', 'img/tenor.gif');
 			modal_header.css('background-color', '#d64f02')
 			modal.modal();
 		} else {
-			modal_title.text('You did it!!! You\'ve done the impossible!!! That.Was.EPIC!!!');
+			modal_title.text('You did it!!! You\'ve done the impossible!!! That.Was.AMAZING!!!');
 			image.attr('src', 'img/cheer.gif');
 			modal_header.css('background-color', '#d60202')
 			modal.modal();
-			setTimeout(redirect, 6500);
+			setTimeout(redirect, 7200);
 		}
 	} else {
 		$('#response_div').text('Please only use a number between 1-10.');
@@ -66,6 +67,7 @@ function make_guess() {
 function addModalCloseHandler(){
 	$('.modal-body>img').click(function(){
 		$('#galleryModal').modal('hide');
+		$('#galleryModal .modal-title').css('background-color', '');
 	})
 }
 
@@ -75,5 +77,4 @@ function addModalCloseHandler(){
 
 function redirect() {
 	document.location.href = 'win.html';
-
 }
